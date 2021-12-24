cd64d876225db83bf1ae343f646685ba409a7eb1
alexo <the.onga@gmail.com>
2021-12-25 04:28:43 +1100

    new: Add fire and water packet captures

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import time
 import subprocess
 from enum import Enum
 import os
+import json
 
+def get_raw_commands():
+    with open("raw_packet.json") as f:
+        data = json.load(f)
+    return data
+
+RAW_COMMANDS = get_raw_commands()
 class LightCommand(Enum):
     ON = 1
     OFF = 2
@@ -11,6 +18,8 @@ class LightCommand(Enum):
     BLUE = 4
     GREEN = 5
     PURPLE = 6
+    FIRE = 7
+    WATER = 8
 
 LIGHT_IP = "192.168.1.114"
 BASE_CMD = f"python -m flux_led.fluxled {LIGHT_IP}"
@@ -20,13 +29,18 @@ RED_CMD = f"{BASE_CMD} -c Red"
 BLUE_CMD = f"{BASE_CMD} -c Blue"
 GREEN_CMD = f"{BASE_CMD} -c Green"
 PURPLE_CMD = f"{BASE_CMD} -c Purple"
+FIRE_CMD = f"{BASE_CMD} -r {RAW_COMMANDS['fire']}"
+WATER_CMD = f"{BASE_CMD} -r {RAW_COMMANDS['water']}"
+print(FIRE_CMD)
 
 CommandMap = {LightCommand.ON: ON_CMD,
               LightCommand.OFF: OFF_CMD,
               LightCommand.RED: RED_CMD,
               LightCommand.BLUE: BLUE_CMD,
               LightCommand.GREEN: GREEN_CMD,
-              LightCommand.PURPLE: PURPLE_CMD}
+              LightCommand.PURPLE: PURPLE_CMD,
+              LightCommand.FIRE: FIRE_CMD,
+              LightCommand.WATER: WATER_CMD}
 
 def get_led_library_cwd():
     return os.path.abspath("flux_led")
@@ -54,7 +68,7 @@ def main():
 
 if __name__ == '__main__':
     t = time.time()
-    for i in range (3,7):    
+    for i in range (3,9):    
         run_led_command(LightCommand(i))
         print(time.time() - t)
         t = time.time()
