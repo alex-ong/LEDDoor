bcb16adc37e2583c073ecf8999385ae77ca5f3ee
alexo <the.onga@gmail.com>
2021-12-24 21:42:11 +1100

    Simple map of commands implemented

--- main.py
+++ main.py
@@ -13,17 +13,27 @@ class LightCommand(Enum):
     PURPLE = 6
 
 LIGHT_IP = "192.168.1.114"
-BASE_CMD = f"python -m flux_led.fluxled -i {LIGHT_IP}"
+BASE_CMD = f"python -m flux_led.fluxled {LIGHT_IP}"
 ON_CMD = f"{BASE_CMD} -1"
 OFF_CMD = f"{BASE_CMD} -0"
-LS_CMD = "ls"
+RED_CMD = f"{BASE_CMD} -c Red"
+BLUE_CMD = f"{BASE_CMD} -c Blue"
+GREEN_CMD = f"{BASE_CMD} -c Green"
+PURPLE_CMD = f"{BASE_CMD} -c Purple"
+
+CommandMap = {LightCommand.ON: ON_CMD,
+              LightCommand.OFF: OFF_CMD,
+              LightCommand.RED: RED_CMD,
+              LightCommand.BLUE: BLUE_CMD,
+              LightCommand.GREEN: GREEN_CMD,
+              LightCommand.PURPLE: PURPLE_CMD}
 
 def get_led_library_cwd():
     return os.path.abspath("flux_led")
 
 def run_led_command(command):
-    if command == LightCommand.ON:
-        pass
+    string_cmd = CommandMap[command]
+    run_subprocess(string_cmd)
     
 def run_subprocess(command):
     cwd = get_led_library_cwd()
@@ -43,4 +53,10 @@ def main():
         time.sleep(0.5)
 
 if __name__ == '__main__':
-    run_subprocess(ON_CMD)
+    t = time.time()
+    for i in range (3,7):    
+        run_led_command(LightCommand(i))
+        print(time.time() - t)
+        t = time.time()
+
+    run_led_command(LightCommand.OFF)
