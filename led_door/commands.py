24839747c76e23bdc69c60c04cc2f142438f200e
alexo <the.onga@gmail.com>
2025-01-23 21:26:09 +1100

    new: Add functionality to do a global disable
    Also format with ruff

--- led_door/commands.py
+++ led_door/commands.py
@@ -1,11 +1,16 @@
 import json
 from enum import Enum
+
+
 def get_raw_commands():
     with open("raw_packet.json") as f:
         data = json.load(f)
     return data
 
+
 RAW_COMMANDS = get_raw_commands()
+
+
 class LightCommand(Enum):
     FIRE = -1
     WATER = -2
@@ -22,11 +27,14 @@ COLOR_CMD = f"{BASE_CMD} -c"
 FIRE_CMD = f"{BASE_CMD} -r {RAW_COMMANDS['fire']}"
 WATER_CMD = f"{BASE_CMD} -r {RAW_COMMANDS['water']}"
 
-CommandMap = {LightCommand.ON: ON_CMD,
-              LightCommand.OFF: OFF_CMD,
-              LightCommand.COLOR: COLOR_CMD,
-              LightCommand.FIRE: FIRE_CMD,
-              LightCommand.WATER: WATER_CMD}
-              
+CommandMap = {
+    LightCommand.ON: ON_CMD,
+    LightCommand.OFF: OFF_CMD,
+    LightCommand.COLOR: COLOR_CMD,
+    LightCommand.FIRE: FIRE_CMD,
+    LightCommand.WATER: WATER_CMD,
+}
+
+
 def ColorCommand(color):
     return (LightCommand.COLOR, color)
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+from enum import Enum
 import time
 import subprocess
 import os
 import logging
 
 from PIL import Image
+from pystray import Icon, Menu, MenuItem
 from webcam_monitor.status import check_status
 from led_door.commands import CommandMap, LightCommand, ColorCommand
 
-logging.basicConfig(filename='logging.log',
-                    format='%(asctime)s %(levelname)-8s %(message)s',
-                    level=logging.INFO,
-                    datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(
+    filename="logging.log",
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
 def get_led_library_cwd():
     return os.path.abspath("flux_led")
+
 
 def run_led_command(command):
     if isinstance(command, LightCommand):
@@ -20,20 +27,24 @@ def run_led_command(command):
     else:
         string_cmd = CommandMap[command[0]] + " " + command[1]
     run_subprocess(string_cmd)
-    
+
+
 def run_subprocess(command):
     cwd = get_led_library_cwd()
     subprocess.run(command.split(), cwd=cwd)
 
-def is_in_work_meeting(items):
-    return any(item in ["teams","skype","webex", "zoom"] for
-               item in items)
 
-def handle_new_status(status, last_command):
+def is_in_work_meeting(items):
+    return any(item in ["teams", "skype", "webex", "zoom"] for item in items)
+
+
+def handle_new_status(status, last_command, is_force_disabled):
     logging.info(status)
     webcam, mic = status
     command = LightCommand.OFF
-    if len(webcam) > 0:    
+    if is_force_disabled:
+        pass
+    elif len(webcam) > 0:
         # Always run fire or water if webcam is on
         all_items = webcam + mic
         if is_in_work_meeting(all_items):
@@ -49,50 +60,82 @@ def handle_new_status(status, last_command):
             command = ColorCommand("255, 37, 255")
         else:
             command = ColorCommand("Blue")
-    
+
     if command != last_command:
         run_led_command(command)
-        
+
     return command
-        
-    
+
 
 def main(icon):
     run_led_command(LightCommand.ON)
     last_sent_ts = time.time()
     last_command = None
     last_status = None
+    last_force_off = is_force_off_checked()
     icon.visible = True
     while not icon._led_stop:
         new_status = check_status()
-        if new_status != last_status or time.time() - last_sent_ts > 60:
-            last_command = handle_new_status(new_status,
-                                             last_command)
+        if (
+            new_status != last_status
+            or time.time() - last_sent_ts > 60
+            or last_force_off != is_force_off_checked()
+        ):
+            last_command = handle_new_status(
+                new_status, last_command, is_force_off_checked()
+            )
             last_status = new_status
+            last_force_off = is_force_off_checked
             last_sent_ts = time.time()
         time.sleep(0.016)
 
-def create_image():
-    # Generate an image and draw a pattern
-    image = Image.open("RGBDoor.png")
+
+class ImageEnum(Enum):
+    RGBDoor = "RGBDoor.png"
+    Black = "BlackDoor.png"
+
+
+def create_image(image_enum: ImageEnum = ImageEnum.RGBDoor) -> Image:
+    image = Image.open(image_enum.value)
     return image
+
 
 def close_app(icon, _):
     icon._led_stop = True
     icon.stop()
-    
 
-import pystray
-from pystray import Icon, Menu, MenuItem
+
+FORCE_OFF = False
+
+
+def toggle_force_off(icon, _):
+    global FORCE_OFF
+    FORCE_OFF = not FORCE_OFF
+    if FORCE_OFF:
+        icon.icon = create_image(ImageEnum.Black)
+    else:
+        icon.icon = create_image(ImageEnum.RGBDoor)
+
+
+def is_force_off_checked(_=None):
+    global FORCE_OFF
+    return FORCE_OFF
+
+
 def create_icon():
-    icon = Icon('LEDDoor', create_image(), 
-        menu=Menu(MenuItem('Close',
-                close_app)))
-    
+    icon = Icon(
+        "LEDDoor",
+        create_image(),
+        menu=Menu(
+            MenuItem("Force Disable", toggle_force_off, checked=is_force_off_checked),
+            MenuItem("Close", close_app),
+        ),
+    )
+
     return icon
 
 
-if __name__ == '__main__':
-    icon = create_icon()  
-    icon._led_stop = False      
+if __name__ == "__main__":
+    icon = create_icon()
+    icon._led_stop = False
     icon.run(main)
